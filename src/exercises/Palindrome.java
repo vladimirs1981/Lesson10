32b2461564ce210a9d38e057e063b16b35c9b405
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-07-07 18:09:32 +0200

    Palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,29 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import examples.FileHelper;
 
 public class Palindrome {
 
+	List<String> words = new ArrayList<String>();
+
+	Palindrome() {
+
+		words = loadWords();
+	}
+
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	}
+
+	public boolean wordExist(String string) {
+
+		return words.contains(string);
+	}
+
+	public boolean wordIsAPalindrome(String string) {
+		return wordExist(string) && wordExist(new StringBuilder(string).reverse().toString());
 	}
 }
