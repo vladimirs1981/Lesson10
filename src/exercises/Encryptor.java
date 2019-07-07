b12c70e95577da28d96b59e6823eb7b981d3b58f
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-07-07 18:07:12 +0200

    Encryptor class

--- src/exercises/Encryptor.java
+++ src/exercises/Encryptor.java
@@ -3,14 +3,26 @@ package exercises;
 public class Encryptor {
 
 	public static String encrypt(String encryptMe) {
-		
+
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < encryptMe.length(); i++) {
 			int numericalValue = encryptMe.charAt(i);
-			numericalValue+=2;
+			numericalValue += 2;
 			builder.append((char) numericalValue);
 		}
 		return builder.toString();
+	}
+
+	public static String decrypt(String decryptMe) {
+
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < decryptMe.length(); i++) {
+			int numericalValue = decryptMe.charAt(i);
+			numericalValue -= 2;
+			builder.append((char) numericalValue);
+		}
+		return builder.toString();
+
 	}
 
 }
