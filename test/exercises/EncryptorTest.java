07219e30b68396a6c8a4ef5d1c72cb7ced89ead0
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-07-07 18:07:41 +0200

    Encryptor test

--- test/exercises/EncryptorTest.java
+++ test/exercises/EncryptorTest.java
@@ -1,6 +1,6 @@
 package exercises;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,8 +16,11 @@ class EncryptorTest {
 		assertEquals("ccc", Encryptor.encrypt("aaa"));
 		assertEquals("Lcxc", Encryptor.encrypt("Java"));
 	}
+	@Test
+	void testDecryptWord() throws Exception{
+		assertEquals("Live long and prosper!", Encryptor.decrypt("Nkxg\"nqpi\"cpf\"rtqurgt#"));
+	}
 	
-	// Exercise: decrypt this sentence: Nkxg"nqpi"cpf"rtqurgt#
 	
 	
 }
